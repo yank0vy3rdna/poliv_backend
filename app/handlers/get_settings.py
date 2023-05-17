776e9cb475120232aa4c21cd980286898de6dace
Andrey Kryukov <yankovyerdna@yandex.ru>
2023-05-17 21:11:52 +0300

    Update get_settings.py

--- app/handlers/get_settings.py
+++ app/handlers/get_settings.py
@@ -23,27 +23,30 @@ async def h(unique_str: str, info_from_device: str):
     rules.sort()
     settings_str = [1 if device.enabled else 0, len(rules) * 4]
     for rule in rules:
-        if rule + config.TIME_OF_NABOR + config.TIME_OF_POLIV <= 24 * 60:
-            settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
-            settings_str.append(rule >> 8)
-            settings_str.append(config.POLIV_RELAY_PIN)
-            settings_str.append(1)
+        if rule + config.TIME_OF_NABOR + config.TIME_OF_DELAY + config.TIME_OF_POLIV > 24 * 60:
+            continue
+        settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
+        settings_str.append(rule >> 8)
+        settings_str.append(config.POLIV_RELAY_PIN)
+        settings_str.append(1)
 
-            rule += config.TIME_OF_POLIV
-            settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
-            settings_str.append(rule >> 8)
-            settings_str.append(config.POLIV_RELAY_PIN)
-            settings_str.append(0)
+        rule += config.TIME_OF_POLIV
+        settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
+        settings_str.append(rule >> 8)
+        settings_str.append(config.POLIV_RELAY_PIN)
+        settings_str.append(0)
 
-            settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
-            settings_str.append(rule >> 8)
-            settings_str.append(config.NABOR_RELAY_PIN)
-            settings_str.append(1)
+        rule += config.TIME_OF_DELAY
 
-            rule += config.TIME_OF_NABOR
-            settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
-            settings_str.append(rule >> 8)
-            settings_str.append(config.NABOR_RELAY_PIN)
-            settings_str.append(0)
+        settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
+        settings_str.append(rule >> 8)
+        settings_str.append(config.NABOR_RELAY_PIN)
+        settings_str.append(1)
+
+        rule += config.TIME_OF_NABOR
+        settings_str.append(rule - (rule >> 8 << 8))  # little-endian uint16_t
+        settings_str.append(rule >> 8)
+        settings_str.append(config.NABOR_RELAY_PIN)
+        settings_str.append(0)
     settings_str.extend([0] * (162 - len(settings_str)))
     return Response(content=bytes(settings_str), media_type="application/macbinary")
